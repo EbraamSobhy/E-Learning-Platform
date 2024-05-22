47a6a52398373aa5778cd49c57f89dec8c58e156
EbraamSobhy <142206373+EbraamSobhy@users.noreply.github.com>
2024-05-22 19:05:01 +0300

    add footer in tutorial page

--- src/content/Toturials/Toturials.jsx
+++ src/content/Toturials/Toturials.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import './tutorials.css';
+import Footer from "../../components/Footer/Footer";
 
 function Tutorials() {
     useEffect(() => {
@@ -7,25 +8,25 @@ function Tutorials() {
     }, []);
 
     const videoCategories = {
-        "Front-End Development": [
+        "Front-End Development :": [
             'https://www.youtube.com/embed/sBws8MSXN7A',
             'https://www.youtube.com/embed/1Rs2ND1ryYc',
             'https://www.youtube.com/embed/W6NZfCO5SIk',
             'https://www.youtube.com/embed/pQN-pnXPaVg'
         ],
-        "Back-End Development": [
+        "Back-End Development :": [
             'https://www.youtube.com/embed/fBNz5xF-Kx4',
             'https://www.youtube.com/embed/F5mRW0jo-U4',
             'https://www.youtube.com/embed/tN6oJu2DqCM?si=nKlZIerdzoSVdtj7',
             'https://www.youtube.com/embed/7S_tz1z_5bA'
         ],
-        "Mobile App Development": [
+        "Mobile App Development :": [
             'https://www.youtube.com/embed/x0uinJvhNxI',
             'https://www.youtube.com/embed/0-S5a0eXPoc',
             'https://www.youtube.com/embed/F9UC9DY-vIU',
             'https://www.youtube.com/embed/comQ1-x2a1Q'
         ],
-        "Cyber Security": [
+        "Cyber Security :": [
             'https://www.youtube.com/embed/inWWhr5tnEA?si=GtRYeLTvVoxDzXCo',
             'https://www.youtube.com/embed/KZ93C-CroAA?si=tKeOuyc1UZz38mAO',
             'https://www.youtube.com/embed/JbEPJv7Ybcs?si=aKREIR5I5mTyh2yQ',
@@ -55,6 +56,8 @@ function Tutorials() {
                     </div>
                 </div>
             ))}
+            <br />
+            <Footer />
         </div>
     );
 }
